27c2baef432fb4108b80c82423d2173e46de3d58
neitan-02 <hernandezmorenonathan09@gmail.com>
2025-09-25 02:06:11 -0600

    configuracion de server.js para produccion

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors());  // Habilita CORS para todas las peticiones
+// Middlewares
+app.use(cors()); 
 app.use(express.json());
 
 // Rutas existentes
@@ -19,11 +19,11 @@ app.use("/api/tarea", tarea);
 const sesionRoutes = require("./routes/sesion");
 app.use("/api/sesion", sesionRoutes);
 
-const tareaRoutes = require('./routes/progreso');
-app.use('/api/progreso', tareaRoutes);
+const tareaRoutes = require("./routes/progreso");
+app.use("/api/progreso", tareaRoutes);
 
-const logroRoutes = require('./routes/logro');
-app.use('/api/logro', logroRoutes);
+const logroRoutes = require("./routes/logro");
+app.use("/api/logro", logroRoutes);
 
 const logrosUnlockedRoutes = require("./routes/logro_unlocked");
 app.use("/logros-unlocked", logrosUnlockedRoutes);
@@ -31,21 +31,21 @@ app.use("/logros-unlocked", logrosUnlockedRoutes);
 const maestroRoutes = require("./routes/maestros");
 app.use("/api/maestros", maestroRoutes);
 
-
-
-////////////////////////Aqui tenemos los endpoints de la ia
+// Endpoints de IA
 const iaRoutes = require("./routes/ia_routes");
 app.use("/api", iaRoutes);
 
-///////////////////////Aqui termina los endpoints dela ia
 // Conexión a MongoDB
 const mongoURI = process.env.MONGO_URI;
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Conexión a MongoDB exitosa'))
-    .catch((err) => console.error('Error al conectar a MongoDB:', err));
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ Conexión a MongoDB exitosa"))
+  .catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
 
-// Puerto del servidor
+// Puerto y Host del servidor
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+const HOST = process.env.HOST || "0.0.0.0"; 
+
+app.listen(PORT, HOST, () => {
+  console.log(`🚀 Servidor corriendo en http://${HOST}:${PORT}`);
 });
